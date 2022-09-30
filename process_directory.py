b3ddeafd6cd805edb6046d568a6831247f1bfe3d
Benjamin Chang <benjaminchang23@gmail.com>
2022-09-30 18:35:07 -0400

    feat: initial commit

---
+++ process_directory.py
@@ -0,0 +1,86 @@
+import argparse
+import json
+import os
+from typing import List
+
+from re import match as reg_match
+from re import search as reg_search
+
+parser = argparse.ArgumentParser(description="modify directory structure of mango output.")
+parser.add_argument("--inputfields", required=True, help="JSON file containing all the required fields.")
+args = parser.parse_args()
+
+def ReadInputJson(jsonFile):
+    fp = open(jsonFile, "r")
+    inputs = json.load(fp)
+    target_directory = inputs.get("target_directory")
+    return target_directory
+
+def GetChapterChapterNum(chapter_name : str) -> str:
+    search_res = reg_search("^.+ Vol\. [0-9]+ Ch\. ([0-9]+?)\.cbz$", chapter_name)
+    if search_res is not None:
+        return search_res.group(1)
+
+    return None
+
+def GetChapterTitle(chapter_name : str) -> str:
+    search_res = reg_search("^(.+?) Vol\. [0-9]+ Ch\. [0-9]+\.cbz$", chapter_name)
+    if search_res is not None:
+        return search_res.group(1)
+
+    return None
+
+def GetChapterVolumeNum(chapter_name : str) -> str:
+    search_res = reg_search("^.+ Vol\. ([0-9]+?) Ch\. [0-9]+\.cbz$", chapter_name)
+    if search_res is not None:
+        return search_res.group(1)
+
+    return None
+
+def GetSeriesList(target_directory : str) -> List[str]:
+    return [f.path for f in os.scandir(target_directory) if f.is_dir()]
+
+def GetUnmodifiedChapterList(target_directory : str, series_name : str) -> List[str]:
+    return [f.path for f in os.scandir(target_directory) if IsUnmodifiedChapter(f, series_name)]
+
+def IsUnmodifiedChapter(dir_entry : str, series_name : str) -> bool:
+    path_str = dir_entry.path
+    # only get things that do not start with series name
+    if reg_match("^.+ Vol\. [0-9]+ Ch\. [0-9]+\.cbz$", path_str) and not reg_match("^{} Vol\. [0-9]+ Ch\. [0-9]+ .+.cbz$".format(series_name), path_str):
+        return True
+    return False
+
+if __name__ == "__main__":
+    # read json
+    (target_directory) = ReadInputJson(args.inputfields)
+
+    # get all dir names as titles
+    series_paths = GetSeriesList(target_directory)
+
+    print("found {} series".format(len(series_paths)))
+
+    for series_path in series_paths:
+        series_name = os.path.basename(series_path)
+        # print("series fullpath: {}".format(series_path))
+        print("series name: {}".format(series_name))
+        chapter_list = GetUnmodifiedChapterList(series_path, series_name)
+
+        # modify each title chapter to -> title - vol/ch num - chapter title
+        for chapter_path in chapter_list:
+            print("processing: {}".format(chapter_path))
+            chapter_name = os.path.basename(chapter_path)
+
+            # regex search for vol/ch name and title
+            volume_number_str = GetChapterVolumeNum(chapter_name)
+            chapter_number_str = GetChapterChapterNum(chapter_name)
+            chapter_title_str = GetChapterTitle(chapter_name)
+
+            if chapter_number_str is None or chapter_title_str is None  or volume_number_str is None:
+                print("warn: series: {} chapter: {} fail regex search".format(series_name, chapter_name))
+                continue
+
+            new_name = str(series_name + " Vol. " + volume_number_str + " Ch. " + chapter_number_str + " " + chapter_title_str + ".cbz")
+            print("new: {}".format(new_name))
+            abs_new = os.path.join(os.path.dirname(chapter_path), new_name)
+            os.rename(chapter_path, abs_new)
+            
