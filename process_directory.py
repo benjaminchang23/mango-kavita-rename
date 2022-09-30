fd74dcb85cc0d3b426af3357a4f53b666396b56a
Benjamin Chang <benjaminchang23@gmail.com>
2022-09-30 19:38:18 -0400

    feat: adjust secondary regex check for unmodified

--- process_directory.py
+++ process_directory.py
@@ -48,7 +48,7 @@ def IsUnmodifiedChapter(dir_entry : str, series_name : str) -> bool:
     # only get things that do not start with series name
     if reg_match("^.*Vol\. [0-9]+ Ch\. [0-9\.]+\.cbz$", path_str) and not reg_match("^{} Vol\. [0-9]+ Ch\. [0-9\.]+.*\.cbz$".format(series_name), path_str):
         return True
-    if reg_match("^.*Ch\. [0-9\.]+\.cbz$", path_str) and not reg_match("^{} Ch\. [0-9\.]+.*\.cbz$".format(series_name), path_str):
+    if reg_match("^.*Ch\. [0-9\.]+\.cbz$", path_str) and not reg_match("^{}.*Ch\. [0-9\.]+.*\.cbz$".format(series_name), path_str):
         return True
     return False
 
